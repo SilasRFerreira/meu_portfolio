c2adf9a77780a9911e35425d1250b291f7570e07
SilasRFerreira <silasrf1@gmail.com>
2023-12-15 10:35:55 -0300

    styled: applyed styles to head

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,14 +2,14 @@ import Link from "next/link"
 
 const Header = () => {
     return(
-        <header>
-            <div>
-                <h1>Meu Portfólio</h1>
-                <nav>
-                    <Link href={"/"}>Home</Link>
-                    <Link href={"#about"}>Sobre mim</Link>
-                    <Link href={"#projects"}>Projetos</Link>
-                    <Link href={"#contacts"}>Contatos</Link>
+        <header className="h-20 w-full flex items-center justify-center fixed">
+            <div className="w-11/12 lg:w-4/5 flex justify-between">
+                <h1 className="text-xl font-bold">Meu Portfólio</h1>
+                <nav className="space-x-8">
+                    <Link href={"/"} className="text-base font-bold">Home</Link>
+                    <Link href={"#about"} className="text-base font-medium">Sobre mim</Link>
+                    <Link href={"#projects"} className="text-base font-medium">Projetos</Link>
+                    <Link href={"#contacts"} className="text-base font-medium">Contatos</Link>
                 </nav>
             </div>
         </header>
