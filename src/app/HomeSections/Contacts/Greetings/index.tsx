59aca06dc1a9736f4c236d3bad8a0fe61203813c
SilasRFerreira <silasrf1@gmail.com>
2023-12-15 14:08:30 -0300

    styled: applyed atyles to greeting section

--- src/app/HomeSections/Contacts/Greetings/index.tsx
+++ src/app/HomeSections/Contacts/Greetings/index.tsx
@@ -1,7 +1,8 @@
 const Greetings = () => {
     return(
-        <div>
-            <h2>Vamos inicar uma conversa e <span>desenvokver nossa criatividade</span>juntos?</h2>            
+        <div className="max-w-md flex items-center align-center">
+            <h2 className="font-bold text-xl">Vamos inicar uma conversa e 
+            <span className="text-primary-blue"> desenvolver nossa criatividade</span> juntos?</h2>            
         </div>
     )
 }
