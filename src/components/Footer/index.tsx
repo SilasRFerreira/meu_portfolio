86677c84123f72dffb844db451b2c220c6bcf19d
SilasRFerreira <silasrf1@gmail.com>
2023-12-15 19:43:09 -0300

    style: added styles to footer as dictated by figma model

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,17 +5,23 @@ import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
 
 const Footer = () => {
     return(
-        <footer>
-            <div>
-                <Image src={profile} alt={"Foto de perfil"} className="h-12 w-12"/>
-                <div>
-                    <h3>Obrigado!</h3>
-                    <p>Siga-me nas mídias sociais e vamos conversar</p>
+        <footer className="w-full flex items-center justify-center">
+            <div className="my-14 w-11/12 lg:w-4/5 flex flex-wrap justify-between max-sm:justify-center gap-8">                
+                <div className="flex flex-wrap justify-center items-center space-x-4 max-w-lg gap-6">
+                    <Image src={profile} alt={"Foto de perfil"} className="h-12 w-12 rounded-full"/>
+                    <div className="max-sm:text-center">
+                        <h3>Obrigado!</h3>
+                        <p>Siga-me nas mídias sociais e vamos conversar</p>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <FontAwesomeIcon icon={faGithub} size="sm" className="h-12"/>
-                <FontAwesomeIcon icon={faLinkedin} className="h-12" />
+                <div className="flex space-x-8 w-32 justify-between">
+                    <a href="https://github.com/SilasRFerreira" target="_blank">
+                        <FontAwesomeIcon icon={faGithub} size="sm" className="h-12 hover:h-14"/>
+                    </a>
+                    <a href="https://www.linkedin.com/in/silas-ferreira-1b4552101/" target="_blank">
+                        <FontAwesomeIcon icon={faLinkedin} className="h-12 hover:h-14"/>
+                    </a>
+                </div>
             </div>
         </footer>
     )
